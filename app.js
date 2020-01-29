8a55c0a0f9f64f9dbfa344d63bd6bd45f0c42ab4
Max Chen <maxchen290@gmail.com>
2020-01-28 23:44:24 -0500

    fix server selection timeout

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var express = require("express"),
 // mongoose.connect("mongodb://localhost:27017/campusites", {useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect("mongodb+srv://max:HUANhuan123@cluster0-ognw1.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
-    useUnifiedTopology: true,
     useCreateIndex:true
 }).then(() => {
     console.log("connected to DB");
@@ -23,7 +22,6 @@ mongoose.connect("mongodb+srv://max:HUANhuan123@cluster0-ognw1.mongodb.net/test?
     console.log("error", err.message);
 });
 
-mongoose.Promise = global.Promise
 
 var app = express();
 const port = 3000;
