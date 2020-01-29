efe538c47c9a236b6221f4b874183c1bb8d7c7da
Max Chen <maxchen290@gmail.com>
2020-01-28 23:03:25 -0500

    changed to mongodb atlas

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var express = require("express"),
     flash = require("connect-flash"),
     seed = require("./seed");
 
-mongoose.connect("mongodb://localhost:27017/campusites", {useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://localhost:27017/campusites", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://max:HUANhuan123@cluster0-ognw1.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
 var app = express();
 const port = 3000;
 
